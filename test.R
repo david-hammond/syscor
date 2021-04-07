c2f4fd1a5fa065a9f1fc521647824d6293b46e2d
David <dhammond@economicsandpeace.org>
2021-04-07 04:12:54 +0000

    testing2

--- test.R
+++ test.R
@@ -1,10 +1,23 @@
-library(systr)
+library(ProjectTemplate)
+library(hammond)
 library(tidyverse)
-library(scales)
-library(corrr)
-df = readRDS("./data/original_data/sdgs.rds")
-meta = readRDS("./data/original_data/sdg_meta.rds")
-newscale = c(1,10)
-systr_setup(df, meta, newscale)
-corpus = readRDS("02-scaled_db.rds") %>% filter(geocode == "MEX")
-systr_granger(corpus)
+library(systr)
+#install.packages("magick")
+library(crop)
+#only uncomment if need to updat from database
+hammond::hdb_login(host = "192.168.0.67", db = "nationalhdb_march2021", user = "admin", password = "admin")
+x = hdb_search("iep") %>% filter(from_file %in% c("02-ppi.R", "01-gpi.R"))
+pos = grepl("Banded", x$seriescode) | x$from_file == "02-ppi.R"
+x = x[pos,]
+# pos = !grepl(":", x$seriescode) | x$from_file == "01-gpi.R"
+# x = x[pos,]
+meta = x %>% select(uid, variablename)
+#corpus = corpus %>% left_join(hammond::hcountryinfo) %>% filter(region == "Central America And Caribbean")
+corpus = hdb_get(x) %>% select(uid, geocode, year, value) %>% filter(geocode != "PSE")
+#corpus = corpus %>% left_join(hammond::hcountryinfo) %>% filter(region == "Central America And Caribbean") %>% select(uid, geocode, year, value)
+#setwd("./data")
+newscale = c(1,5)
+
+systr_setup(corpus, meta, newscale)
+corpus = corpus %>% filter(geocode == "MEX")
+mex = systr_granger(mex)
